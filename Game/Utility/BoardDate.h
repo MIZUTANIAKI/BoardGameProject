aeecdd569c76ff093960e213034cb2f22f747bd2
Aki_School_PC <1916043@PC64228>
2022-07-27 11:17:33 +0900

    コメント

--- Game/Utility/BoardDate.h
+++ Game/Utility/BoardDate.h
@@ -8,11 +8,35 @@ class BoardDate
 {
 	static std::array<std::array<Unit, 8>, 8> boardDate_;
 public:
+	/// <summary>
+	/// 座補をもとにデータを取得
+	/// </summary>
+	/// <param name="x"></param>
+	/// <param name="y"></param>
+	/// <returns></returns>
 	static Unit GatDate(int& x, int& y);
+	/// <summary>
+	/// ボード自体を取得
+	/// </summary>
+	/// <returns></returns>
 	static std::array<std::array<Unit, 8>, 8> GetBoard();
 
+	/// <summary>
+	/// データ設定　陣営単位で初期化
+	/// </summary>
+	/// <param name="isPlayer"></param>
+	/// <param name="date"></param>
 	void SetDate(bool isPlayer, std::vector<int> date);
+	/// <summary>
+	/// データ設定 ボード本体上書き
+	/// </summary>
+	/// <param name="dat"></param>
 	void SetDate(std::array<std::array<Unit, 8>, 8> dat);
+	/// <summary>
+	/// データ設定　ユニット移動用/移動元を移動先へ
+	/// </summary>
+	/// <param name="from"></param>
+	/// <param name="to"></param>
 	void SetDate(Vector2 from, Vector2 to);
 
 	BoardDate();
